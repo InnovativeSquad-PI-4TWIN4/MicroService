0e238bceb4a6873bc671351297a301db7e83f50f
mouadhfersi25 <mouadh.fersi@esprit.tn>
2025-04-07 22:24:59 +0100

    integration avec table userr

--- transportService/src/main/java/com/easytrip/userservice/models/Transport.java
+++ transportService/src/main/java/com/easytrip/userservice/models/Transport.java
@@ -1,32 +1,32 @@
-package com.easytrip.userservice.models;
+    package com.easytrip.userservice.models;
 
-import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+    import lombok.*;
+    import org.springframework.data.annotation.Id;
+    import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
+    import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Document(collection = "transports")
-public class Transport {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Document(collection = "transports")
+    public class Transport {
 
-    @Id
-    private String id;
-    private String voyageId; // ou Long si tu veux référencer un voyage depuis Mongo
+        @Id
+        private String id;
+        private String voyageId; // ou Long si tu veux référencer un voyage depuis Mongo
 
-    private String type;
-    private String compagnie;
-    private int capacite;
-    private String numero;
-    private String villeDepart;
-    private String villeArrivee;
-    private LocalDateTime dateDepart;
-    private LocalDateTime dateArrivee;
-    private double prix;
+        private String type;
+        private String compagnie;
+        private int capacite;
+        private String numero;
+        private String villeDepart;
+        private String villeArrivee;
+        private LocalDateTime dateDepart;
+        private LocalDateTime dateArrivee;
+        private double prix;
 
-    // 🔗 Lien vers l'utilisateur (MySQL)
-    private Long userId;
-}
+        // 🔗 Lien vers l'utilisateur (MySQL)
+        private Long userId;
+    }
